f4be30919db1f94fb449708f38d0730ff02ca899
Cody De Arkland <codydearkland@gmail.com>
2018-11-08 03:41:54 -0800

    updated with delete function

--- vmworldpy.py
+++ vmworldpy.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 def authvc(api_url,user,password):
     r = requests.post(f'{api_url}/rest/com/vmware/cis/session', auth=(user,password), verify=False)
@@ -10,8 +11,8 @@ def authvc(api_url,user,password):
         return print("Fail")
 
 def getapidata(path):
-    sid = authvc('https://hlcorevc01.humblelab.com','administrator@vsphere.local','VMware123!')
-    r = requests.get(f'https://hlcorevc01.humblelab.com/rest{path}', headers={'vmware-api-session-id':sid}, verify=False)
+    sid = authvc('https://vcenter.sddc-34-218-57-180.vmc.vmware.com',os.environ['user'],os.environ['password'])
+    r = requests.get(f'https://vcenter.sddc-34-218-57-180.vmc.vmware.com/rest{path}', headers={'vmware-api-session-id':sid}, verify=False)
     print(r.json())
     if r.status_code == 200:
         return r.json()
@@ -20,18 +21,37 @@ def getapidata(path):
 
 def lightswitch(vmname):
     data = getapidata('/vcenter/vm')
-    sid = authvc('https://hlcorevc01.humblelab.com','administrator@vsphere.local','VMware123!')
+    sid = authvc('vcenterhere',os.environ['user'],os.environ['password'])
     for i in data['value']:
         if i['name'] == vmname:
             id = i['vm']
             print("The ID for this VM is "+id)
-            pstate = requests.get(f'https://hlcorevc01.humblelab.com/rest/vcenter/vm/{id}/power', headers={'vmware-api-session-id':sid}, verify=False)
+            pstate = requests.get(f'vcenterhere/rest/vcenter/vm/{id}/power', headers={'vmware-api-session-id':sid}, verify=False)
             print("Power State Data is as follows "+pstate.json()['value']['state'])
             if pstate.json()['value']['state'] == 'POWERED_OFF':
-                state = requests.post(f'https://hlcorevc01.humblelab.com/rest/vcenter/vm/{id}/power/start', headers={'vmware-api-session-id':sid}, verify=False)
+                state = requests.post(f'vcenterhere/rest/vcenter/vm/{id}/power/start', headers={'vmware-api-session-id':sid}, verify=False)
                 print("Starting VM")
                 return state.status_code
             else:
-                state = requests.post(f'https://hlcorevc01.humblelab.com/rest/vcenter/vm/{id}/power/stop', headers={'vmware-api-session-id':sid}, verify=False)
+                state = requests.post(f'vcenterhere/rest/vcenter/vm/{id}/power/stop', headers={'vmware-api-session-id':sid}, verify=False)
+                print("Stopping VM")
+                return state.status_code
+
+def deletevm(vmname):
+    data = getapidata('/vcenter/vm')
+    sid = authvc('vcenterhere',os.environ['user'],os.environ['password'])
+    for i in data['value']:
+        if i['name'] == vmname:
+            id = i['vm']
+            print("The ID for this VM is "+id)
+            pstate = requests.get(f'vcenterhere/rest/vcenter/vm/{id}/power', headers={'vmware-api-session-id':sid}, verify=False)
+            print("Power State Data is as follows "+pstate.json()['value']['state'])
+            if pstate.json()['value']['state'] == 'POWERED_OFF':
+                state = requests.delete(f'vcenterhere/rest/vcenter/vm/{id}', headers={'vmware-api-session-id':sid}, verify=False)
+                print("Starting VM")
+                return state.status_code
+            else:
+                state = requests.post(f'vcenterhere/rest/vcenter/vm/{id}/power/stop', headers={'vmware-api-session-id':sid}, verify=False)
+                delete = requests.delete(f'vcenterhere/rest/vcenter/vm/{id}', headers={'vmware-api-session-id':sid}, verify=False)
                 print("Stopping VM")
                 return state.status_code
